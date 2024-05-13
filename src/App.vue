16af7230eb680525afc24105c50345fc9ddb591d
victorsantanna <victor.esef@hotmail.com>
2024-05-13 13:59:51 -0300

    feat: importa o useCounterStore

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>Welcome to pinia</h2>
+  {{ counter.showCount }}
+
+  <button @click="counter.increment">Increment</button>
+
+
+</template>
+
+<script setup>
+import {useCounterStore} from './stores/counterStore'; 
+const counter = useCounterStore();
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
